19f45a4d6c6b0291463a63e36e21bf463f49cef2
Dharul Mittal <dharulmittal@gmail.com>
2021-09-04 14:01:07 +0530

    Added time display

--- Water/Exercise.py
+++ Water/Exercise.py
@@ -1,6 +1,7 @@
 from pygame import mixer
 mixer.init()
 from time import time , sleep
+from datetime import datetime
 run = "y"
 start_time = time()
 samay = eval(input("\nTime after which u want 2 Move\n"))
@@ -15,11 +16,12 @@ while run =="y":
         mixer.music.load("Thunder.mp3")
         mixer.music.play(-1)
         while a !="y" and a !="re" and  a != "change" and a != "reset" and a!="exit":
-            a = input("Have u done\n")
+            a = input(f"Have u done - [{datetime.now()}]\n")
             a = a.lower()
             if a == "y":
                 start_time = time()
                 mixer.music.pause()
+                print(f"Done at + {datetime.now()}\n")
             elif a=="re" or a == "change" or a == "reset":
                 start_time = time()
                 mixer.music.pause()
@@ -28,7 +30,7 @@ while run =="y":
             elif a=="exit":
                 run == n
             elif a == "n" :
-                print("\nTho kar na bsdk\n")
+                print("\nTho hill le na bsdk\n")
                 continue
             else:
                 continue 

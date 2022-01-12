8c1bac8c14eaf232becdf9f0dd2de15f62f40183
Nalla Prakash <81852531+prakashNalla@users.noreply.github.com>
2022-01-12 23:31:46 +0530

    revamp and pythonify game.py

--- Stone paper scissors/Game.py
+++ Stone paper scissors/Game.py
@@ -1,51 +1,107 @@
+#!/usr/bin/env python3.10
 import random
 
-run = "y"
-round = 1 
-player_points = 0
-CPU_points = 0
-draw = 0 
-l = ["Stone","Paper","scissors"]
+INCREASE_SCORE, DECREASE_SCORE = 1, 0
+OPTIONS = ["stone","paper","scissors"]
+PLAYER, CPU, DRAW, NUMBER_OPTIONS = 0, 0, 0, len(OPTIONS)
+DEFAULT_RUN_GAME, DEFAULT_NUMBER_ROUNDS = 'n', 3
+# messages to be displayed
+WON_MESSAGE, LOST_MESSAGE, DRAW_MESSAGE = "You Won!", "You Lost", "Drawn"
 
-while run == "y":
-    while(round <= 10) :    
-        try :
-            print(f"Round = {round}")
-            cpu_choose = random.choice(l)
-            player_choose = int(input("1 for stone\n2 For Paper\n3 For Scissors\n"))
-            player_choose2 = l[player_choose - 1 ]
-            if player_choose2 == cpu_choose :
-                print("Match draw\n")
-                draw += 1
-            elif player_choose == 1 and cpu_choose ==l[3 -1]:
-                print(f"You Won")
-                player_points += 1 
-            elif player_choose == 1 and cpu_choose ==l[2 - 1]:
-                print(f"You lost")
-                CPU_points += 1 
-            elif player_choose == 2 and cpu_choose ==l[3 - 1]:
-                print(f"You lost")
-                CPU_points += 1 
-            elif player_choose == 2 and cpu_choose ==l[1 - 1]:
-                print(f"You Won")
-                player_points += 1 
-            elif player_choose == 3 and cpu_choose ==l[1 - 1]:
-                print(f"You Lost")
-                CPU_points += 1 
-            elif player_choose == 3 and cpu_choose ==l[2 - 1]:
-                print(f"You Won")
-                player_points += 1 
-            else :
-                print("Wrong input try Again")
-        except Exception as p:
-            print(p)
-        round += 1
-  
-    if player_points > CPU_points :
-        print(f"You won\nYour score = {player_points}\nCPU Score = {CPU_points}\nDraw = {draw}")
-    elif player_points < CPU_points :
-        print(f"CPU won\nYour score = {player_points}\nCPU Score = {CPU_points}\nDraw = {draw}")
-    elif player_points == CPU_points :
-        print(f"Draw\nYour score = {player_points}\nCPU Score = {CPU_points}\nDraw = {draw}")
-    run = input("press 'y' to play again and 'n' to exit")
-print("\nThx for playing\n")
+NUMBER_ROUNDS = input("Number of rounds you want -> ")
+if not NUMBER_ROUNDS.isdecimal():
+    NUMBER_ROUNDS = DEFAULT_NUMBER_ROUND
+else:
+    NUMBER_ROUNDS = int(NUMBER_ROUNDS)
+
+if not isinstance(
+        (RUN_GAME := input("Want to play? (y/n) -> ")[0].lower()),
+        str
+    ) or RUN_GAME != 'y':
+    RUN_GAME = DEFAULT_RUN_GAME
+
+
+def welcome():
+    """ Returns Welcome Message """
+    return (
+            "Welcome to Stone/Paper/Scissors!\n"
+            "1 -> Stone\n"
+            "2 -> Paper\n"
+            "3 -> Scissor"
+        )
+
+def game_logic(
+    player_score=PLAYER,
+    cpu_score=CPU,
+    draw_score=DRAW):
+    """ Run the Stone/Paper/Scissors game """
+    if not isinstance(
+        (player := int(input("what's your bet? -> "))),
+        int
+    ) or player not in range(1, NUMBER_OPTIONS+1):
+        return
+    # random option(chosen by the random module)
+    cpu = random.randrange(1, NUMBER_OPTIONS)
+    if player == cpu:
+        draw_score += INCREASE_SCORE
+        print(DRAW_MESSAGE)
+    elif player - cpu in range(1, NUMBER_OPTIONS+1):
+        print(WON_MESSAGE)
+        player_score += INCREASE_SCORE
+        cpu_score -= DECREASE_SCORE
+    elif player - cpu in range(-1, -1*(NUMBER_OPTIONS+1), -1):
+        print(LOST_MESSAGE)
+        cpu_score+= INCREASE_SCORE
+        player_score-= DECREASE_SCORE
+    else: # sure nothing would go wrong, but still
+        print(player, cpu)
+    # program ran successfully without any exceptions
+    return player_score, cpu_score, draw_score
+
+
+def final_scores( player_score, cpu_score, draw_score):
+    """ Prints Final ScoreBoard"""
+    baseline_scoreboard = (
+        f"Player Score -> {player_score}\n"
+        f"CPU Score -> {cpu_score}\n"
+        f"Draw Score -> {draw_score}"
+    )
+    if player_score > cpu_score:
+        return (
+            f"{WON_MESSAGE}\n"
+            f"{baseline_scoreboard}"
+        )
+    if cpu_score > player_score:
+        return (
+            f"{LOST_MESSAGE}\n"
+            f"{baseline_scoreboard}"
+        )
+    return (
+        f"{DRAW_MESSAGE}\n"
+        f"{baseline_scoreboard}"
+    )
+
+
+def main():
+    """ Main Function """
+    global PLAYER, CPU, DRAW
+    quit_game = False
+    # start game
+    print(welcome())
+    while RUN_GAME == 'y':
+        for _ in range(NUMBER_ROUNDS):
+            scores = game_logic()
+            if scores is None:
+                break
+            PLAYER, CPU, DRAW = map(
+                    lambda score: sum(score),
+                    zip((PLAYER, CPU, DRAW), scores)
+                )
+        if not scores is None:
+            # print(PLAYER, CPU, DRAW)
+            print(f"\n{final_scores(PLAYER, CPU, DRAW)}")
+        break
+
+
+if __name__ == '__main__':
+    main()

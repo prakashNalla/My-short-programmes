ed3d1502f748d10d21a9ce5a738963286dae9013
Nalla Prakash <81852531+prakashNalla@users.noreply.github.com>
2022-01-13 23:47:04 +0530

    minor bug fixes, made code more concise

--- Stone paper scissors/Game.py
+++ Stone paper scissors/Game.py
@@ -7,23 +7,12 @@ has to implemented
 """
 
 INCREASE_SCORE, DECREASE_SCORE = 1, 0  # increase/decrease score
+CONFIRM_RUN = 'y'
 OPTIONS = ["stone","paper","scissors"]  # all options as of now
 PLAYER, CPU, DRAW, NUMBER_OPTIONS = 0, 0, 0, len(OPTIONS)  # scores & no. of options
 DEFAULT_RUN_GAME, DEFAULT_NUMBER_ROUNDS = 'n', 3  # fallback variables
 # messages to be displayed
 WON_MESSAGE, LOST_MESSAGE, DRAW_MESSAGE = "You Won!", "You Lost", "Drawn"  # messages
-
-NUMBER_ROUNDS = input("Number of rounds you want -> ")  # input number of rounds
-if not NUMBER_ROUNDS.isdecimal():  # check if given no. is a int
-    NUMBER_ROUNDS = DEFAULT_NUMBER_ROUND
-else:
-    NUMBER_ROUNDS = int(NUMBER_ROUNDS)
-
-if not isinstance(
-        (RUN_GAME := input("Want to play? (y/n) -> ")[0].lower()),
-        str
-    ) or RUN_GAME != 'y': # check if input credentials are correct
-    RUN_GAME = DEFAULT_RUN_GAME
 
 
 def welcome():
@@ -35,26 +24,48 @@ def welcome():
             "3 -> Scissor"
         )
 
+
+def get_run_game_confirmation():
+    """ Returns response from player """
+    # check if input credentials are correct
+    if not (run_game := input("Want to play? (y/n) -> ").lower()) == CONFIRM_RUN:
+        return DEFAULT_RUN_GAME
+    return run_game
+
+
+def get_number_rounds():
+    """ Return number of rounds player inputs """
+    number_rounds = input("Number of rounds you want(3) -> ")  # input number of rounds
+    if not number_rounds.isdecimal():  # check if given no. is a int
+        return DEFAULT_NUMBER_ROUNDS
+    return int(number_rounds)
+
+
 def game_logic(
     player_score=PLAYER,
     cpu_score=CPU,
     draw_score=DRAW):
     """ Run the Stone/Paper/Scissors game """
-    if not isinstance(
-        (player := int(input("what's your bet? -> "))),
-        int
-    ) or player not in range(1, NUMBER_OPTIONS+1):  # get and check input option
+    if (
+        player := int(input("what's your bet? -> "))
+        ) not in range(1, NUMBER_OPTIONS+1):  # get and check input option
         return
     # random option(chosen by the random module)
-    cpu = random.randrange(1, NUMBER_OPTIONS)
+    cpu = random.randrange(1, NUMBER_OPTIONS+1)
+    print(
+        ("==>-==>-===-<==-<==\n"
+        f"PLAYER: {OPTIONS[player-1].title()}\n"
+        f"CPU:    {OPTIONS[cpu-1].title()}\n"
+        "==>-==>-===-<==-<==")
+        )
     if player == cpu:  # if draw
         draw_score += INCREASE_SCORE
         print(DRAW_MESSAGE)
-    elif player - cpu in range(1, NUMBER_OPTIONS+1):  # if player won
+    elif player - cpu in (1, -2):
         print(WON_MESSAGE)
         player_score += INCREASE_SCORE
         cpu_score -= DECREASE_SCORE
-    elif player - cpu in range(-1, -1*(NUMBER_OPTIONS+1), -1): # if player lost
+    elif player - cpu in (-1, 2): # if player lost
         print(LOST_MESSAGE)
         cpu_score+= INCREASE_SCORE
         player_score-= DECREASE_SCORE
@@ -92,21 +103,21 @@ def main():
     global PLAYER, CPU, DRAW
     quit_game = False
     # start game
-    print(welcome())
-    while RUN_GAME == 'y':
-        for _ in range(NUMBER_ROUNDS): # run game till given output
-            scores = game_logic() # stat game
+    print(welcome(), "\n")
+    while get_run_game_confirmation() == CONFIRM_RUN:
+        for _ in range(get_number_rounds()): # run game till given output
+            scores = game_logic() # start game
             if scores is None:
                 break
             PLAYER, CPU, DRAW = map(
                     lambda score: sum(score),
-                    zip((PLAYER, CPU, DRAW), scores) 
+                    zip((PLAYER, CPU, DRAW), scores)
                 ) # set and update scores
         if not scores is None:  # print final scorecard
             # print(PLAYER, CPU, DRAW)
             print(f"\n{final_scores(PLAYER, CPU, DRAW)}")  # final scores
         break  # break the loop once done
-    
+
 
 
 if __name__ == '__main__': # run
